a2466bd552d66153a9ee87294f491edd8ce08ed3
M Haidar Alawie <mohamad.alawie@kcl.ac.uk>
2020-06-11 16:26:07 +0100

    add admin privileges

--- src/glow/api/viewsets.py
+++ src/glow/api/viewsets.py
@@ -7,22 +7,61 @@ from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 
+#user model viewset
+class CommentViewSet(viewsets.ViewSet):
+	queryset = Comment.objects.all()
+	serializer_class = CommentSerializer
+	
+	#create comment
+	def create(self, request, *args, **kwargs):
+		requested = request.data
+		requested= Comment.objects.create(
+			user=request.user,
+			content= requested["content"]
+			)
+		requested.save()
+		serializer= CommentSerializer(requested)
+		return Response(serializer.data)
+	
+	#list all comment
+	def list(self, request):
+		queryset = Comment.objects.all()
+		serializer = CommentSerializer(queryset, many=True)
+		return Response(serializer.data)
+
+	#retrieve a single comment 
+	def retrieve(self, request, pk=None):
+		queryset = Comment.objects.all()
+		comment = get_object_or_404(queryset, pk=pk)
+		serializer = CommentSerializer(comment)
+		return Response(serializer.data)
+
+	def destroy(self, request, pk=None):
+		if request.user.is_staff:
+			Comment.objects.filter(pk=pk).delete()
+			return Response("delete successful")
+		return Response("YOU ARE NOT AN ADMIN")
+
 
 # Blog viewset
 class BlogViewSet(viewsets.ViewSet):
+	#blog queryset
 	queryset = Blog.objects.all()
 	serializer_class = BlogSerializer
 
 	def create(self, request, *args, **kwargs):
 		requested = request.data
-		requested= Blog.objects.create(
-			user=request.user,
-			title= requested["title"],
-			content= requested["content"]
-			)
-		requested.save()
-		serializer= BlogSerializer(requested)
-		return Response(serializer.data)
+		if request.user.is_staff:
+			requested= Blog.objects.create(
+				user=request.user,
+				title= requested["title"],
+				content= requested["content"]
+				)
+			requested.save()
+			serializer= BlogSerializer(requested)
+			return Response(serializer.data)
+		return Response("Access denied: You are not an admin")
+
 	#update blog post 
 	def update(self, request,pk=None, *args, **kwargs):
 		queryset = Blog.objects.all()
@@ -49,15 +88,11 @@ class BlogViewSet(viewsets.ViewSet):
 		serializer = BlogSerializer(blog)
 		return Response(serializer.data)
 
-
-class Comment(viewsets.ViewSet):
-		#retrieve a single comment 
-	def retrieve(self, request, pk=None):
-		queryset = Comment.objects.all()
-		comment = get_object_or_404(queryset, pk=pk)
-		serializer = CommentSerializer(blog)
-		return Response(serializer.data)
-
+	def destroy(self, request, pk=None):
+		if request.user.is_staff:
+			Blog.objects.filter(pk=pk).delete()
+			return Response("delete successful")
+		return Response("Access denied: You are not an admin")
 
 #user model viewset
 class UserViewSet(viewsets.ModelViewSet):
--- src/glow/glow/router.py
+++ src/glow/glow/router.py
@@ -1,6 +1,10 @@
-from api.viewsets import UserViewSet, BlogViewSet
+from api.viewsets import UserViewSet, BlogViewSet, CommentViewSet
 from rest_framework import routers
 
+#router
 router= routers.DefaultRouter()
+#register view sets
+router.register('Comment', CommentViewSet, basename="comment")
 router.register('User', UserViewSet)
 router.register('Blog', BlogViewSet, basename="blog")
+
