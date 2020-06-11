1bc3f4e122f0b15c4c15ba733d134af5848d6586
M Haidar Alawie <mohamad.alawie@kcl.ac.uk>
2020-06-11 15:56:33 +0100

    add many to one field in comment

--- src/glow/api/models.py
+++ src/glow/api/models.py
@@ -9,11 +9,18 @@ class Blog(models.Model):
 	content = models.TextField(max_length=500)
 	date = models.DateTimeField(auto_now_add=True)
 	modified_date= models.DateTimeField(auto_now_add=True, editable=True)
+	
+	def __str__(self):
+		return self.title
 
 class Comment(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	content = models.CharField(max_length=100)
 	date = models.DateTimeField(auto_now_add=True)
+	related_post = models.ForeignKey(Blog, on_delete=models.CASCADE)
+	
+	def __str__(self):
+		return self.content
 
 
 
